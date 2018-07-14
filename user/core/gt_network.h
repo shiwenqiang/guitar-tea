551b06687873bd98ab1fdf541e9fdf7a762a1712
shiwenqiang <vincishi@gmail.com>
2018-07-14 16:08:18 +0800

    clean and fix some bug

---
+++ user/core/gt_network.h
@@ -0,0 +1,21 @@
+/**************************************************************
+ *          Copyright 2015. Guitar&Tea Tech. Co., Ltd.
+ *                     All Rights Reseved
+ *
+ * File     :gt_network.h
+ * Module   :Network Engine
+ * Date     :2017/10/11
+ * Author   :shiwenqiang
+ * Brief    :Implementation the network.
+ *
+ *------------------------Revision History----------------------
+ *  No. Version Date        Mender        Desc.
+ *  1   V0.01   2017-10-11  shiwenqiang   Original Version.
+ **************************************************************/
+
+#ifndef __GT_NETWORK_H__
+#define __GT_NETWORK_H__
+
+extern int32_t gt_network_init(void);
+
+#endif /* __GT_NETWORK_H__　*/
