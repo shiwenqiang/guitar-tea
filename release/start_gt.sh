d13302f86c99394212ca35f3214c51b5a96460b2
shiwenqiang <vincishi@gmail.com>
2018-12-27 21:34:30 +0800

    modify & improve shell code

--- release/start_gt.sh
+++ release/start_gt.sh
@@ -3,8 +3,10 @@
 echo "Start GT domain..."
 
 cd ../build/bin/
-./gt 192.168.1.102 5555
 
-if [ $? -ne 0 ] ; then
-    echo "Failed to start GT. " $?
+./gt 192.168.1.102 5555 > /dev/null 2>&1
+result=$?
+
+if [ $result -ne 0 ] ; then
+    echo "Failed to start GT. ret:" $result
 fi
--- release/stop_gt.sh
+++ release/stop_gt.sh
@@ -4,4 +4,9 @@ echo "Stop GT domain..."
 
 cd ../build/bin/
 
-kill -9 `pidof gt` 
+kill -9 `pidof gt` > /dev/null 2>&1
+result=$?
+
+if [ $result -ne 0 ]; then
+    echo "Failed to stop GT. ret:" $result
+fi
